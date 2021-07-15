04cf2d00574c439fa56d1520b9a3ff97d645dd17
hrobbin <idcdtokms@gmail.com>
2021-07-15 15:57:18 +0300

    old files

--- regexp/match_search2.py
+++ regexp/match_search2.py
@@ -41,3 +41,25 @@ result = re.sub(r'#.*$', '', testStr)
 print(result)
 result = re.sub(r'\D', '', testStr)
 print("Ready phone number:", result)
+
+# re.subn returns the string with replaced chars and the number of replacements
+
+my_regex_str = '200.10.2.0 255.255.255.0 200.20.5.2 1 205 T#1 S IB 5'
+a = re.search(r"(.+?) +\d\d(\d)\.([0-9]{2,})\.([0-9]{1,3})\.(\d) +(.+)1 +(\d{3}) +(\w{1})#.+S(\s+)(\w)\w +(.*)", my_regex_str)
+
+print(a.group(0))
+
+
+regex_str = "123.456.789   0 PYTHON 3"
+
+regex = re.search(r"(.+)\s{2,}", regex_str)
+
+print(regex.group(1))
+
+regex = re.search(r"(.+)\s{2,}", regex_str)
+
+print('|%s|' % regex.group(1))
+
+regex = re.sub(r"[a-zA-Z0-9]", '%', regex_str)
+
+print('|%s|' % regex)
