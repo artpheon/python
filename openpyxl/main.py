e301dacc1a71f65fb634ae5f4678095203431314
hrobbin <idcdtokms@gmail.com>
2021-07-27 18:20:09 +0300

    added table creator

--- openpyxl/main.py
+++ openpyxl/main.py
@@ -1,5 +1,7 @@
 import openpyxl
+from openpyxl import Workbook
 from openpyxl.styles import *
+from openpyxl.worksheet.table import Table, TableStyleInfo
 
 
 def show_table(sheet: openpyxl.worksheet.worksheet.Worksheet):
@@ -38,22 +40,55 @@ def test2():
     workbook = openpyxl.load_workbook('./sample.xlsx')
     sheet = workbook['FoodSales']
     cell = sheet['B8']
-
     font = Font(color=colors.BLUE,
                 bold=True,
                 italic=True)
-
     cell.font = font
-
     fill = PatternFill(fill_type='solid',
                        bgColor='F7FE2E')
-
     cell.fill = fill
-
     workbook.save('./sample_COLOURED.xlsx')
     workbook.close()
 
 
+def get_records():
+    text_file = open("./new_table.txt")
+    records = []
+    text_file.seek(0)
+    for record in text_file.readlines():
+        records.append(record.rstrip('\n').split(','))
+    text_file.close()
+    return records
+
+
+def fill_workbook(records):
+    workbook = Workbook()
+    fpath = "./new_table.xlsx"
+    workbook.save(fpath)
+    sheet = workbook['Sheet']
+    sheet.title = 'Flights'
+
+    for row in records:
+        sheet.append(row)
+
+    table = Table(displayName='Table', ref='A1:H8')
+    style = TableStyleInfo(name='TableStyleMedium9', showRowStripes=True, showColumnStripes=True)
+    table.tableStyleInfo = style
+
+    sheet.add_table(table)
+
+    font = Font(color=colors.BLUE, bold=True, italic=True)
+    for cell_no in range(2, 8):
+        if int(sheet['D%s' % cell_no].value) % 10 > 3:
+            sheet['D%s' % cell_no].font = font
+    workbook.save(fpath)
+    workbook.close()
+
+
+def create_from_txt():
+    records = get_records()
+    fill_workbook(records)
+
+
 if __name__ == '__main__':
-    test()
-    test2()
+    create_from_txt()
